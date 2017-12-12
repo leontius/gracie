613c5f8cb7c86f3a7c14c0e340db6944f177c4c5
zhangyi <zhangyi@geexfinance.cn>
2017-12-12 17:47:12 +0800

    first commit

---
+++ gracie-db-compare/src/main/java/org/fx/gracie/db/compare/Application.java
@@ -0,0 +1,28 @@
+package org.fx.gracie.db.compare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Leon
+ * Date: 17/12/11
+ * Time: 下午5:07
+ * Description:
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ gracie-db-compare/src/main/java/org/fx/gracie/db/compare/config/Config.java
@@ -0,0 +1,16 @@
+package org.fx.gracie.db.compare.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Leon
+ * Date: 17/12/11
+ * Time: 下午5:39
+ * Description:
+ */
+@ComponentScan
+@Configuration
+public class Config {
+}
